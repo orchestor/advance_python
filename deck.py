9cbc328e5260e4c2f292253e5eaddcf59603a2e5
Lin Wang <orchestor@gmail.com>
2019-05-10 12:13:38 +0800

    change format

--- deck.py
+++ deck.py
@@ -5,17 +5,19 @@ Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class Deck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = ['spades diamonds clubs hearts'.split(' ', 1)
+    suits = ['spades diamonds clubs hearts'.split()]
 
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits
                        for rank in self.ranks]
+
     def __len__(self):
         return len(self._cards)
+
     def __getitem__(self, position):
         return self._cards[position]
 
 
 if __name__ == '__main__':
-             deck = Deck()
-             print(len(deck))
+    deck = Deck()
+    print(len(deck))
